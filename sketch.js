164b499e5f2f8c7defdad65ff7b666079c846f9a
Zishi-debug <tuzishizhenkeai@gmail.com>
2023-01-22 22:22:38 +0000

    week4

--- sketch.js
+++ sketch.js
@@ -1,59 +1,130 @@
-/*
-  A grid of Mohr patterns
-  Programming for artists and designers
-  Joe McAlister
-*/
-
-let f = 0.0;
 let spc;
 let numIters;
+let a;
+let block;
+let gape;
+let v = [];
+let c = [];
+let f = 0.0
+let writer;
 
-function setup()
-{
+function setup() {
   createCanvas(800, 800);
-  
-  // our grid spacing
-  numIters = 10; 
+   writer=createWriter('newFile.txt');
+  numIters = 20;
   spc = width / numIters;
+  a = spc / 2;
+  block = createGraphics(2 * a, 2 * a);
+  block.clear();
+  
+  for (let x = 0; x < 6; x++) {
+    
+    v[x] = x*(sqrt(3) * a) / 4 ;
+    print('The value of v is ' + v[x]);
+  }
+  for (let y =0; y<6;y++){
+    c[y] = y*(2 * a) / 4;
+    print('The value of c is ' + c[y]);
+  }
 }
 
 
-function draw()
-{
+
+function draw() {
   background(255);
-  
-  for (let i = 0; i < numIters; i++) {
-    for (let j = 0; j < numIters; j++) {
-      // push and pop matrix so that we dont cumulatively translate 
+  //stave
+  //image(block,0,0);
+  //line(0,0,200,200);
+  for (let i = 0; i < numIters; i ++) {
+    for (let j = 0; j < numIters; j ++) {
       push();
+      //scale size
+      translate(width/4,height/6);
       
-      // move to the current location in the grid
-      translate(spc/2 + i * spc, spc/2 +  j * spc);
-      
-      // call a function which draws something at (0, 0)
-      drawAMohr( i + 1, j + 1);
+      scale(0.5,0.5);
+      push();
+      // 30 is gap
+      gape = (sqrt(3) * a)/2;
+      translate( i * spc,  j * sqrt(3) * a +j*gape);
+      image(block, 0, 0);
+      drawLine(i+1,j+1);
+      pop();
       
       pop();
     }
   }
-  
-  // itterate f so we have an animation
-  f+= 0.01;
-}
+  // draw stave;
+  for (let k = 0; k <5; k ++) {
+    
+    block.stroke(0);
+    block.strokeWeight(0.5);
 
-
-/*
-  This function uses perlin noise aka noise();
-  Unlike random(), noise() will return consistent results for the same parameters
-  This means it's good for animation
-  https://processing.org/reference/noise_.html
-*/
-function drawAMohr(i, j)
-{
-  beginShape();
-  for (let k = 0; k < 15; k++) {
-    vertex(noise( k * j, i + f) * spc  - spc/2, noise( i, j *k + f ) * spc - spc/2);
-      print('The value of A is ' + i);
+    block.line(2, k*sqrt(3)/4 * a+1, spc-2 , k*sqrt(3)/4 * a+1);
+    
   }
-  endShape();
+//   f += 0.01
+  
+  
 }
+
+
+ function drawLine(i,j){
+   //k = how many lines in one block
+   for (let k = 0; k <4; k++){
+     //let A = vertex(noise(k*j,i)*spc,v[int(noise(i,k*j)*5)])
+     let l;
+     push();
+     strokeWeight(int(noise(k*j+1,i)*4));
+     noFill();
+    
+     translate(c[int(noise(k*j,i+f)*6)],v[int(noise(i+f,k*j)*6)]);
+     print('The value of startpoint.x is ' + noise(k*j,i));
+     print('The value of startpoint.y is ' + int(noise(i,k*j)*6));
+     rotate(int(noise(k*j,i)*6)*radians(60));
+     strokeCap(SQUARE);
+     
+     l= int(noise(k*j,i)*4)*(a/2);
+     
+     line(0,0,l+1,0);
+     //circle(0,0,4);
+     fill(0);
+     writer.write(' ',-c[int(noise(k*j,i+f)*6)],-v[int(noise(i+f,k*j)*6)]);
+     if (l==0){
+       //strokeWeight(2);
+       noFill();
+       circle(0,0,4);
+       fill(0);
+       //text('hello',-c[int(noise(k*j,i+f)*6)],-v[int(noise(i+f,k*j)*6)]);
+        //text(' ',-c[int(noise(k*j,i+f)*6)],-v[int(noise(i+f,k*j)*6)]);
+       writer.write('我',-c[int(noise(k*j,i+f)*6)],-v[int(noise(i+f,k*j)*6)]);
+       
+     }
+     
+     //if(r[k]>0.1){
+        //rotate(radians(60)); 
+     //}
+     
+       
+     pop();
+     
+     
+     
+     //line(10,10,30,30);
+     print('The value ofl is ' + int(noise(k*j,i)*4));
+   }
+   }
+
+ function keyPressed(){
+   if(key === 's'){
+      save("img.png");  
+   }
+ 
+   if(key === 'd'){
+     writer.close();
+   }
+
+}
+//   A = noise(0,2*a)+20;
+//   B = v[int(noise(0,4))]+20;
+//   line(10,10,noise(0,2*a),v[int(noise(0,4))]);
+// 
